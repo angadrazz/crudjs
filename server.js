c31e6cbd30d5f2d4570500acf0b5d6a0fcb0b62f
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-25 17:09:56 +0530

    render file are created

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express()
 app.get('/',(req,res) =>{
     console.log("What are you doing?")
     // res.send("Today's i am going to College")
-    res.status(500).json("What is your name")
+    // res.status(500).json("What is your name")
+    res.render("index")
 })
 
 app.listen(3000)
