cb6ada07f1f69df15e119b1fa73f25370578eb69
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-25 16:54:25 +0530

    Bad Request

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 
 app.get('/',(req,res) =>{
     console.log("What are you doing?")
-    res.send("Today's i am going to College")
+    // res.send("Today's i am going to College")
+    res.sendStatus(400)
 })
 
 app.listen(3000)
