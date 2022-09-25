67c43606885b0a46be3563a6123c90e5af7bcab1
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-25 16:52:09 +0530

    how to start nodemon

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-console.log("Hello World!");
-console.log("Hello World!");
-console.log("Hello World!");
+const express = require("express")
+const app = express() 
+
+app.get('/',(req,res) =>{
+    console.log("What are you doing?")
+    res.send("Today's i am going to College")
+})
+
+app.listen(3000)
