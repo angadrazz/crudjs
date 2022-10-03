c538c729f7404892ba17b151c5adddc829256f41
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-10-03 20:42:33 +0530

    asynchronoius

--- newserver.js
+++ newserver.js
@@ -56,3 +56,36 @@ console.log(stack.size());
 
 stack.clear();
 console.log(stack.items);
+
+function download(url, cb){
+    setTimeout(() => {
+    console.log("Movies downloaded");
+    cb(url);    
+    }, 3000);
+}
+
+function upload(url){
+    console.log("Movie uploaded")
+    }
+    
+    
+  download("url", upload);
+
+
+  function getUsers(callback) {
+    setTimeout(() => {
+      callback([
+        { username: 'john', email: 'john@test.com' },
+        { username: 'jane', email: 'jane@test.com' },
+      ]);
+    }, 1000);
+  }
+  
+  function findUser(username, callback) {
+    getUsers((users) => {
+      const user = users.find((user) => user.username === username);
+      callback(user);
+    });
+  }
+  
+  findUser('john', console.log);
